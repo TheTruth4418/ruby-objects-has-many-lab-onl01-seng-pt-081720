1d3df7111651b6c5ced0f7ecf09e359766077c1e
William West <wwest466@gmail.com>
2020-09-13 14:22:30 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,16 +12,16 @@ class Artist
   end
 
   def add_song(song)
-#    @songs << song
-#    song.artist = self
-#    @@song_count += 1
+    @songs << song
+    song.artist = self
+    @@song_count += 1
   end
 
   def add_song_by_name(name)
-#    song = Song.new(name)
-#    @songs << song
-#    song.artist = self
-#    @@song_count += 1
+    song = Song.new(name)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
 
   end
 
