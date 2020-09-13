bc818b7c15f59cd34ab50c2d2394e3fcd0887899
William West <wwest466@gmail.com>
2020-09-13 14:12:36 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,14 +13,14 @@ class Artist
 
   def add_song(song)
     @songs << song
-    song.artist = self
+    song.artist.name = self
     @@song_count += 1
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
     @songs << song
-    song.artist = self
+    song.artist.name = self
     @@song_count += 1
     binding.pry
   end
